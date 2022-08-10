3320ee7daaeb700d420b0b2850bac80728f5a9ca
Bautista Bambozzi <bautistabambozzi@gmail.com>
2022-08-10 18:59:21 -0300

    Finished part0 3

--- part1/exercise_c/src/App.js
+++ part1/exercise_c/src/App.js
@@ -1,27 +1,12 @@
-let curYear = new Date().getFullYear();
-let calendarDateString = new Date().toDateString();
-let myTime = {'year': curYear, 'calendarDate': calendarDateString};
-console.log(myTime.year)
-function App() {
+import {useState} from 'react'
+
+const App = (props) => {
   return (
       <>
-      <h1>Work in Progress!</h1>
-          <ShowTime timeObj={myTime}/>
       </>
-  );
+  )
 }
 
 
-
-const ShowTime = (props) => {
-    let yearNow = props.timeObj.year;
-    return (
-        <>
-    <p>The current year is equal to {yearNow}!</p>
-            <p>The calendar date is {props.timeObj.calendarDate}</p>
-        </>
-    )
-
-}
 
 export default App;
--- part1/exercise_c/src/index.js
+++ part1/exercise_c/src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React , {useState} from "react";
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+let counter = 0;
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App counter={counter}/>
 );
+
+
