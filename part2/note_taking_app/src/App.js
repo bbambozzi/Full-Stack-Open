2ec91a7d12a9b5e306daf9734b4ddee1957580e6
Bautista Bambozzi <bautistabambozzi@gmail.com>
2022-08-26 16:40:47 -0300

    Reformatted the Course
    Deleted leftover files, re-organized the folder structure into a more readable and concise structure.

---
+++ part2/note_taking_app/src/App.js
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import Note from './components/Note'
+
+let App = (props) => {
+
+
+  let notes = props.notes;
+
+  let [getAllNotes, setAllNotes] = useState(notes);
+  let [getNewNote, setNewNote] = useState('');
+  let [showAll, setShowAll] = useState(true)
+
+
+  const notesToShow =
+    showAll ? getAllNotes : getAllNotes.filter(note => (note.important));
+
+
+  let handleNewNoteChange = (event) => {
+    let newNote = event.target.value;
+    setNewNote(newNote);
+
+  }
+  let handleSaveNewNote = (event) => {
+    event.preventDefault();
+    let userInput = getNewNote
+    let newNoteObject = {
+      id: getAllNotes.length + 1,
+      content: userInput,
+      important: Math.random() > 0.5,
+      date: new Date(),
+    };
+    setAllNotes(getAllNotes.concat(newNoteObject));
+    setNewNote("");
+  }
+
+
+
+  return (
+    <>
+      <>
+        <h1>Note-Taking App</h1>
+        <ul>
+          {notesToShow.map((note) => <li key={note.id}><Note content={note.content} /></li>)}
+        </ul>
+        <>
+          <button onClick={() => setShowAll(!showAll)}>
+            {showAll ? 'Important' : 'Everything'}
+          </button>
+        </>
+      </>
+      <>
+        <h2>Add a new Note!</h2>
+        <form onSubmit={handleSaveNewNote}>
+          <input onChange={handleNewNoteChange} value={getNewNote} />
+          <button type="submit">Save!</button>
+        </form>
+      </>
+    </>
+  )
+}
+export default App;
---
+++ part2/note_taking_app/src/components/Note.js
@@ -0,0 +1,8 @@
+const Note = (props) => {
+  let content = props.content;
+  return (
+    <span>{content}</span>
+  );
+}
+
+export default Note
