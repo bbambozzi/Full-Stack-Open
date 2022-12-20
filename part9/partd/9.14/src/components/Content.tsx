bc0da1b58168f54339cc645ed6a7c103c9d43bf1
Bautista Bambozzi <bautistabambozzi@gmail.com>
2022-12-20 00:10:25 -0300

    Finished half of part9d!

--- part9/partd/9.14/src/components/Content.tsx
+++ part9/partd/9.14/src/components/Content.tsx
@@ -6,13 +6,35 @@ const Content = (props: coursePartsProps): JSX.Element => {
   return (
     <>
       {props.courseList.map((c) => {
-        return (
-          <>
-            <p>
-              {c.name} has {c.exerciseCount} exercises.
-            </p>
-          </>
-        );
+        switch (c.type) {
+          case "normal":
+            return (
+              <>
+                <strong>{c.name}</strong>
+                <p>
+                  <em>{c.description}</em>
+                </p>
+              </>
+            );
+
+          case "groupProject":
+            return (
+              <>
+                <strong>{c.name}</strong>
+                <p>exercise number {c.groupProjectCount}</p>
+              </>
+            );
+          case "submission":
+            return (
+              <>
+                <strong>{c.description}</strong>
+                <p>{c.description}</p>
+                <p>submitted at: {c.exerciseSubmissionLink}</p>
+              </>
+            );
+          default:
+            return <h4>This will never evaluate</h4>;
+        }
       })}
     </>
   );
--- part9/partd/9.14/src/types/coursePart.tsx
+++ part9/partd/9.14/src/types/coursePart.tsx
@@ -1,4 +1,25 @@
-export default interface coursePart {
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
+  type: string;
 }
+
+interface CourseNormalPart extends CoursePartBase {
+  type: "normal";
+  description: string;
+}
+
+interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartBase {
+  type: "submission";
+  description: string;
+  exerciseSubmissionLink: string;
+}
+
+type coursePart = CourseNormalPart | CourseSubmissionPart | CourseProjectPart;
+
+export default coursePart;
