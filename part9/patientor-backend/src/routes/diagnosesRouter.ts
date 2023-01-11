d127d3b5ecf8f972f43bebdcf5eb977a1983510e
Bautista Bambozzi <bautistabambozzi@gmail.com>
2023-01-10 23:32:01 -0300

    Finished part9!

--- part9/patientor-backend/src/routes/diagnosesRouter.ts
+++ part9/patientor-backend/src/routes/diagnosesRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 import diagnosisService from "../services/diagnosisService";
 
 router.get("/", (_, res) => {
