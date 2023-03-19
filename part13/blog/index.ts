be771555d5ccd6cbee392fd6b17581ada3b4a945
Bautista Bambozzi <bautistabambozzi@gmail.com>
2023-03-19 13:27:24 -0300

    Finished 13a!

--- part13/blog/index.ts
+++ part13/blog/index.ts
@@ -8,7 +8,6 @@ const DATABASE_URL = process.env.DATABASE_URL || "fix";
 app.use(express.json());
 
 const sequelize = new Sequelize(DATABASE_URL);
-
 class Blog extends Model {}
 
 Blog.init(
@@ -43,6 +42,7 @@ Blog.init(
     modelName: "blog",
   }
 );
+Blog.sync(); // creates table if it doesent exist
 
 app.get("/api/blogs", async (req, res) => {
   const blogs = await Blog.findAll();
@@ -58,6 +58,25 @@ app.post("/api/blogs", async (req, res) => {
     console.error(`Error ${e}`);
     res.status(400).json({ e });
   }
+});
+
+app.get("/api/blogs/:id", async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog) {
+    res.status(200).json(blog);
+    return;
+  } else {
+    res.sendStatus(404);
+    return;
+  }
+});
+
+app.delete("/api/blogs/:id", async (req, res) => {
+  // code goes here
+  const blog = await Blog.findByPk(req.params.id);
+  await blog?.destroy();
+  res.sendStatus(204).end();
+  return;
 });
 
 app.listen(PORT, () => {
